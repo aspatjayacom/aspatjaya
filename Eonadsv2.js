2b059e9a6045063ea0102338f080a455e7163c08
aspatjayacom <59759849+aspatjayacom@users.noreply.github.com>
2020-05-13 08:31:37 +0700

    Add files via upload

---
+++ Eonadsv2.js
@@ -0,0 +1,10 @@
+<style>@media screen and (min-width: 721px){#teaser2{width:auto;height:auto;text-align:left;display:none;position:fixed;bottom:0px;left:0px;}}@media screen and (max-width: 720px){#teaser2{width:auto;height:auto;text-align:left;display:scroll;position:fixed;bottom:0px;left:0px;}}</style>
+
+<script type='text/javascript'>$(document).ready(function(){$(&quot;img#closed&quot;).click(function(){$(&quot;#btm_banner&quot;).hide(90)})});</script>
+<script type='text/javascript'>/*<![CDATA[*/var a=navigator,b="userAgent",c="indexOf",f="&m=1",g="(^|&)m=",h="?",i="?m=1";function j(){var m=window.location.href,l=m.split(h);switch(l.length){case 1:return m+i;case 2:return 0<=l[1].search(g)?null:m+f;default:return null}}if(-1!=a[b][c]("Mobile")&&-1!=a[b][c]("WebKit")&&-1==a[b][c]("iPad")||-1!=a[b][c]("Opera Mini")||-1!=a[b][c]("IEMobile")){var k=j();k&&window.location.replace(k)}/*]]>*/</script><script type='text/javascript'>if(window.jstiming){window.jstiming.load.tick(&quot;headEnd&quot;)};</script>
+<div id='teaser2' style='width:autopx;height:0;text-align:left;display:scroll;position:fixed;top:165px;left:0px'>
+<div><a id='close-teaser' onclick='document.getElementById(&apos;teaser2&apos;).style.display=&apos;none&apos;' style='cursor:pointer'><center><font color='red'>  Click (2X) Close </font></center></a></div>
+<center>
+<a target='_blank' title='Selamat Datang'><img alt='Selamat Datang' height='400px' src='https://3.bp.blogspot.com/-pitO7BagceA/XFVq3xhjUkI/AAAAAAAAXH4/cdYoUoRBzGsaeTMULCv8Kpi9GUtjXZE2QCLcBGAs/s640/a.jpg' width='400px'/></a>
+</center>
+</div>
